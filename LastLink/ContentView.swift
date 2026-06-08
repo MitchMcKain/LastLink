77beaf8009931693e6b5e5f7445df3e42a1d7f55
Mitch McKain <mtm120@pitt.edu>
2026-06-08 14:47:44 -0400

    Initial Commit

---
+++ LastLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LastLink
+//
+//  Created by McKain, Mitch T on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LastLink/LastLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastLinkApp.swift
+//  LastLink
+//
+//  Created by McKain, Mitch T on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LastLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
